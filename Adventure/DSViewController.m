877f06249ec8cc994d8eb116576fa70924b8cb53
Dan Szeezil <szeezild@gmail.com>
2014-03-11 16:26:02 -0500

    committing blank Adventure to git

---
+++ Adventure/DSViewController.m
@@ -0,0 +1,29 @@
+//
+//  DSViewController.m
+//  Adventure
+//
+//  Created by Dan Szeezil on 3/11/14.
+//  Copyright (c) 2014 Dan Szeezil. All rights reserved.
+//
+
+#import "DSViewController.h"
+
+@interface DSViewController ()
+
+@end
+
+@implementation DSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
